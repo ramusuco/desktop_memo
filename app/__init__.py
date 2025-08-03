09c2e2fe11cfe63640ac5b7abb996c9aaaece73f
rmsc1248 <yamamototakatowork@gmail.com>
2025-08-03 14:44:57 +0900

    chore

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-"""DesktopMemo application package."""
-
-from .gui import TaskMemoApp
-
-__version__ = "1.0.0"
-__all__ = ["TaskMemoApp"]
--- main.py
+++
@@ -1,7 +0,0 @@
-"""Main entry point for DesktopMemo application."""
-
-from app import TaskMemoApp
-
-if __name__ == "__main__":
-    app = TaskMemoApp()
-    app.run()
